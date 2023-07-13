c99446de5f02330d5a73eb861de6674f12b2521a
Zufiqqar <57473144+Zufiqqar@users.noreply.github.com>
2023-07-13 15:16:06 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,18 +1,18 @@
 terraform {
-    required_providers {
-        docker = {
-            source = "kreuzwerker/docker"
-            version = "3.0.2"
-        }
-        digitalocean = {
-            source = "digitalocean/digitalocean"
-            version = "2.26.0"
-        }
-        local = {
-            source = "hashicorp/local"
-            version = "2.4.0"
-        }
+  required_providers {
+    docker = {
+        source = "kreuzwerker/docker"
+        version = "3.0.2"
     }
+    digitalocean = {
+        source = "digitalocean/digitalocean"
+        version = "2.26.0"
+    }
+    local = {
+        source = "hashicorp/local"
+        version = "2.4.0"
+    }
+  }
 }
 
 provider docker {
